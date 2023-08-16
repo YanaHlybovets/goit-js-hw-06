c25f32b577bb5e4b1d3bcbdf7027bd02ca097748
YanaHlybovets <yana.hlybovets@gmail.com>
2023-08-16 23:05:18 +0200

    Task 5

---
+++ js/task-05.js
@@ -0,0 +1,9 @@
+// <input type="text" id="name-input" placeholder="Please enter your name" />
+// <h1>Hello, <span id="name-output">Anonymous</span>!</h1>
+
+const textInput = document.querySelector("#name-input");
+const output = document.querySelector("#name-output");
+
+textInput.addEventListener("input", (event) => {
+  output.textContent = event.currentTarget.value;
+});
