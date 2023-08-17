2e95f8e866de4350c379ab34eb4bc316b877ef20
YanaHlybovets <yana.hlybovets@gmail.com>
2023-08-17 22:50:12 +0200

    task 8

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,21 @@
+const form = document.querySelector(".login-form");
 
+form.addEventListener("submit", handleSubmit);
+    
+function handleSubmit(event) {
+  event.preventDefault();
+  const {
+    elements: { email, password }
+  } = event.currentTarget;
+    
+    const user = email.value === "" || password.value === "" ?
+    console.log("Please fill in all the fields!") :
+    {
+      email: email.value,
+      password: password.value
+    };
+    
+    console.log(user); 
+
+  event.currentTarget.reset();
+}
