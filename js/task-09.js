06db9b8b5694676e7a33c80a1053cb60634de1a6
YanaHlybovets <yana.hlybovets@gmail.com>
2023-08-17 23:11:48 +0200

    task 9

--- js/task-09.js
+++ js/task-09.js
@@ -3,3 +3,13 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+const body = document.querySelector("body");
+const btn = document.querySelector("button");
+const color = document.querySelector("span");
+
+btn.addEventListener("click", () => {
+  body.style.backgroundColor = getRandomHexColor();
+  color.textContent = getRandomHexColor();
+
+});
