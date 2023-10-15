369b256b80a051e969566fb669459bda353c6b92
YanaHlybovets <yana.hlybovets@gmail.com>
2023-10-15 16:49:56 +0200

    task-1

--- js/task-01.js
+++ js/task-01.js
@@ -1,12 +1,12 @@
+
 const categories = document.querySelectorAll(".item");
 const categoryNum = `Number of categories: ` + categories.length;
 console.log(categoryNum);
 
 categories.forEach(category => {
-    const header = `Category: ${category.querySelector("h2").textContent}
-    `;
-    const elements = category.lastElementChild.querySelectorAll("li");
-    const elementNum = `Elements: ` + elements.length;
-
-    console.log(header + elementNum)
+    const title = `Category: ${category.querySelector("h2").textContent}`;
+    const elements = `Elements: ${category.lastElementChild.querySelectorAll("li").length}`;
+    console.log(title);
+    console.log(elements);
 });
+
