d85b6c7cd996dcbc7d24b3376048506f9ee2e0e6
YanaHlybovets <yana.hlybovets@gmail.com>
2023-10-23 21:58:24 +0200

    task-6

--- js/task-06.js
+++ js/task-06.js
@@ -2,7 +2,7 @@
 
 const input = document.querySelector("#validation-input");
 input.addEventListener("blur", () => {
-    input.value.length >= input.dataset.length ?
+    input.value.length === Number(input.dataset.length) ?
         input.classList.add("valid") & input.classList.remove("invalid") :
         input.classList.add("invalid") & input.classList.remove("valid");
 });
